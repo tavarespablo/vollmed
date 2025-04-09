84534ddd44581effad95a00c48aca3b68a855c91
Pablo Tavares <ptavares@outlook.com.br>
2025-04-09 22:23:23 +0100

    Simulando navegação em tela

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,11 @@
 Cypress.Commands.add('login', (email, senha) => {
-    cy.visit('/login');
-    cy.get('[data-test="inputLoginEmail"]').type(email);
-    cy.get('[data-test="inputLoginSenha"]').type(senha, { log: false });
-    cy.get('[data-test="botaoTeste"]').should('be.visible').click();
-
+    cy.session([email, senha], () => {
+        cy.visit('/login')
+        cy.get('[data-test="inputLoginEmail"]').type(email)
+        cy.get('[data-test="inputLoginSenha"]').type(senha, { log: false })
+        cy.get('[data-test="botaoTeste"]').should('be.visible').click()
+        cy.location('pathname').should('eq', '/dashboard')
+    });
 });
 
 
